f47f99b32de16bb6494e2ef9dede7af3b219dfd2
Antoshef <antoshef21@gmail.com>
2020-12-28 20:18:36 +0200

    DOM Manipulations

--- DOM/05. Chat-Room/solution.js
+++ DOM/05. Chat-Room/solution.js
@@ -1,5 +1,16 @@
 function solve() {
-   //TODO...
-}
+
+   let chatInput = document.getElementById('chat_input');
+   let buttonElement = document.getElementById('send');
+   buttonElement.addEventListener('click', function() {
+
+      let newDiv = document.createElement('div');
+      newDiv.className = 'message my-message';
+      newDiv.textContent = chatInput.value;
+
+      document.getElementById('chat_messages').appendChild(newDiv);
+      document.getElementById('chat_input').value = '';
+   });
+};
 
 
--- DOM/06. Numpad-Calculator/solution.js
+++ DOM/06. Numpad-Calculator/solution.js
@@ -1,4 +1,55 @@
 function solve() {
 
-    //TODO...
-}
+    let expressionOutput = document.getElementById('expressionOutput');
+    let resultOutput = document.getElementById('resultOutput');
+
+    document.querySelector('.clear').addEventListener('click', clear);
+    document.querySelector('.keys').addEventListener('click', symbolClicked);
+
+
+    function clear() {
+        expressionOutput.textContent = '';
+        resultOutput.textContent = '';
+    };
+
+    function symbolClicked(event) {
+        let buttonPressed = event.target.value;
+
+        switch (buttonPressed) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+            expressionOutput.textContent += ` ${buttonPressed} `;
+            break;
+            case '=':
+                let [leftOperand, operator, rightOperand] = expressionOutput.textContent.split(' ');
+                leftOperand = +leftOperand;
+                rightOperand = +rightOperand;
+                if (!rightOperand || !operator) {
+                    resultOutput.textContent = 'NaN';
+                } else {
+                    let result = 0;
+                    switch (operator) {
+                        case '*':
+                        result = Number(leftOperand) * Number(rightOperand);
+                        break;
+                        case '/':
+                        result = Number(leftOperand) / Number(rightOperand);
+                        break;
+                        case '+':
+                        result = Number(leftOperand) + Number(rightOperand);
+                        break;
+                        case '-':
+                        result = Number(leftOperand) - Number(rightOperand);
+                        break;
+                    };
+                    resultOutput.textContent = Number(result);
+                };
+                break;
+                default:
+                    expressionOutput.textContent += buttonPressed;
+        };
+    };
+
+};
