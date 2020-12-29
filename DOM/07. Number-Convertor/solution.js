008ec9939995368e28cf6beeeae4b0207ed6aa6d
Antoshef <antoshef21@gmail.com>
2020-12-29 09:53:46 +0200

    DOM

--- DOM/07. Number-Convertor/solution.js
+++ DOM/07. Number-Convertor/solution.js
@@ -1,4 +1,33 @@
 function solve() {
 
-    //TODO...
-}
+    let input = document.getElementById('input');
+    let menuToElement = document.getElementById('selectMenuTo');
+    let resultElement = document.getElementById('result');
+    let buttonElement = document.getElementsByTagName('button');
+
+    let binary = document.createElement('option');
+    binary.textContent = 'Binary';
+    binary.value = 'binary';
+    let hexadeicmal = document.createElement('option');
+    hexadeicmal.textContent = 'Hexadeicmal';
+    hexadeicmal.value = 'hexadecimal';
+    menuToElement.appendChild(binary);
+    menuToElement.appendChild(hexadeicmal);
+
+    buttonElement[0].addEventListener("click", function() {
+        let n = Number(input.value);
+        let current = menuToElement.value;
+        
+            if (n < 0) {
+              n = 0xFFFFFFFF + n + 1;
+             } 
+        switch (current)  
+        {  
+        case 'binary':  
+        return resultElement.value = (parseInt(n, 10).toString(2));
+        case 'hexadecimal':  
+        return resultElement.value = (parseInt(n, 10).toString(16).toUpperCase());
+        };
+
+    });
+};
