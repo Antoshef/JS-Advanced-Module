6d71f3b5fa6c55be96ff85f1479edaff6879859f
Antoshef <antoshef21@gmail.com>
2020-12-29 16:20:27 +0200

    DOM

--- DOM Manipulations/01. Sections/script.js
+++ DOM Manipulations/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
-}
+
+   let mainElement = document.getElementById('content');
+   
+   for (let line of words) {
+      let current = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = line;
+      paragraph.style.display = 'none';
+      current.appendChild(paragraph);
+      mainElement.appendChild(current);
+   }
+
+   mainElement.addEventListener('click', displayText);
+
+   function displayText(event) {
+      let childElement = event.target.children[0];
+      childElement.style.display = 'block';
+   };
+};
