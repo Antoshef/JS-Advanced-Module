721433ed74e9a9d8bc28ba7097de913e70f3a4c5
Antoshef <antoshef21@gmail.com>
2020-12-29 17:47:39 +0200

    DOM

--- DOM Manipulations/03. Time-Converter/solution.js
+++ DOM Manipulations/03. Time-Converter/solution.js
@@ -1,4 +1,41 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
-}
+    let inputDays = document.getElementById('days');
+    let inputHours = document.getElementById('hours');
+    let inputMinutes = document.getElementById('minutes');
+    let inputSeconds = document.getElementById('seconds');
+
+    let main = document.getElementsByTagName('main');
+    main = main[0];
+
+    main.addEventListener('click', function (event) {
+        let current = 0;
+        let id = event.target.id;
+        switch (id) {
+            case 'daysBtn':
+                current = inputDays.value;
+                document.getElementById('hours').value = current * 24;
+                document.getElementById('minutes').value = current * 24 * 60;
+                document.getElementById('seconds').value = current * 24 * 60 * 60;
+                break;
+            case 'hoursBtn':
+                current = inputHours.value;
+                document.getElementById('days').value = current / 24;
+                document.getElementById('minutes').value = current * 60;
+                document.getElementById('seconds').value = current * 60 * 60;
+                break;
+            case 'minutesBtn':
+                current = inputMinutes.value;
+                document.getElementById('days').value = current / 24 / 60;
+                document.getElementById('hours').value = current / 60;
+                document.getElementById('seconds').value = current * 60;
+                break;
+            case 'secondsBtn':
+                current = inputSeconds.value;
+                document.getElementById('days').value = current / 24 / 60 / 60;
+                document.getElementById('hours').value = current / 60 / 60;
+                document.getElementById('minutes').value = current / 60;
+                break;
+        }
+    });
+};
