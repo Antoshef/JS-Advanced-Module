2feb8b22a14cd0801a52e5d59ea68ae842128763
Antoshef <antoshef21@gmail.com>
2020-12-29 11:11:56 +0200

    DOM

--- DOM/08. JavaScript-Quizz/solution.js
+++ DOM/08. JavaScript-Quizz/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+  
+  let ulElement = document.getElementsByClassName('current');
+  let listElements = document.getElementsByClassName('quiz-answer');
+  let sectionElements = document.getElementsByTagName('section');
+  let quizElement = document.getElementById('quizzie');
+  let rightAnswers = 0;
+  let i = 0;
+
+  quizElement.addEventListener('click', function(event) {
+
+    let current = (event.target.parentElement.parentElement);
+    console.log(current.getAttribute(quizIndex));
+    if (event.target.innerHTML == 'onmouseclick') {
+      console.log(true);
+    } else if (current.dataset.quizIndex == '4') {
+      console.log(false);
+    }
+  });
+
 }
