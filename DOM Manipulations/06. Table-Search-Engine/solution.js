8829d4e0033579d298836b4e51bfda24c5e97d3f
Antoshef <antoshef21@gmail.com>
2020-12-30 13:44:39 +0200

    DOM

--- DOM Manipulations/06. Table-Search-Engine/solution.js
+++ DOM Manipulations/06. Table-Search-Engine/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-   //TODO...
-}
+   
+   let btn = document.getElementById('searchBtn');
+   let input = document.getElementById('searchField');
+
+
+   btn.addEventListener('click', function(e) {
+      let fieldElements = [...document.querySelectorAll('tbody > tr')];
+      for (let line of fieldElements) {
+         line.classList.remove('select');
+         let arr = Array.from(line.children);
+         arr.map(x => {
+            if (x.innerHTML.includes(input.value)) {
+               line.classList.add('select');
+            } 
+         });
+      };
+      input.value = '';
+   });
+
+};
