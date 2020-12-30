67224e457d5dfcf75142c99bc54c25be13f95892
Antoshef <antoshef21@gmail.com>
2020-12-30 09:38:15 +0200

    DOM Manipulations

--- DOM Manipulations/02. Notification/scripts.js
+++ DOM Manipulations/02. Notification/scripts.js
@@ -1,3 +1,13 @@
 function notify(message) {
     
-}
+    let divNotify = document.getElementById('notification');
+    let paragraphElement = document.createElement('p');
+    paragraphElement.innerHTML = message;
+    divNotify.appendChild(paragraphElement);
+    divNotify.style.display = 'block';
+ 
+    setTimeout(function() {
+        divNotify.style.display = 'none';
+        divNotify.innerHTML = '';
+    },2000)
+};
--- DOM Manipulations/04. Locked-Profile/app.js
+++ DOM Manipulations/04. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
-}
+    
+
+    Array.from(document.getElementsByClassName('profile')).forEach(profile => {
+        let btn = profile.querySelector('button');
+
+        btn.addEventListener('click', () => {
+            let unlocked = profile.getElementsByTagName('input')[1];
+            let extraInfo = profile.querySelector('div');
+            if (unlocked.checked && btn.textContent == 'Show more') {
+                extraInfo.style.display = 'block';
+                btn.textContent = 'Hide it';
+            } else if (unlocked.checked && btn.textContent == 'Hide it'){
+                extraInfo.style.display = 'none';
+                btn.textContent = 'Show more';
+            };
+        });
+    });
+};
