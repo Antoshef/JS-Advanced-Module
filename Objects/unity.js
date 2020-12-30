bbcbb8b521e5cd5bf200e89e0c9535b7dcc160dc
Antoshef <antoshef21@gmail.com>
2020-12-30 23:05:32 +0200

    Object

--- Objects/unity.js
+++ Objects/unity.js
@@ -1,16 +1,28 @@
 class Rat {
     constructor(name) {
-        
+        this.name = name,
+        this.unitedRats = []
     }
     unite(rat) {
-        if (Object.keys(rat) == 2) {
-            this.unitedRats.push(rat);
-        };
+        this.unitedRats.push(rat);
     }
     getRats() {
-        return this.unitedRats.toString;
+        return this.unitedRats;
+    }
+    toString() {
+        if (this.unitedRats.length > 0) {
+            let result = this.name + '\n';
+            this.unitedRats.forEach(rat => {
+                result += '##' + rat.name + '\n';
+            });
+            return result;
+        } else {
+            return this.name;
+        };
     }
 }
+
+
 
 let firstRat = new Rat("Peter");
 console.log(firstRat.toString()); // Peter
@@ -18,7 +30,7 @@ console.log(firstRat.toString()); // Peter
 console.log(firstRat.getRats()); // []
 
 firstRat.unite(new Rat("George"));
-firstRat.unite(new Rat("Alex"));
+firstRat.unite(new Rat("fake rat"));
 console.log(firstRat.getRats());
 // [ Rat { name: 'George', unitedRats: [] },
 //  Rat { name: 'Alex', unitedRats: [] } ]
