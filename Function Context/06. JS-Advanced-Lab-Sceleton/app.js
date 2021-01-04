447992f964548cce0321791b12fa43ba4491cbb7
Antoshef <antoshef21@gmail.com>
2021-01-04 07:20:55 +0200

    Context

--- Function Context/06. JS-Advanced-Lab-Sceleton/app.js
+++ Function Context/06. JS-Advanced-Lab-Sceleton/app.js
@@ -1,3 +1,21 @@
 function solve() {
-    // ToDo
-}
+
+    document.querySelector('#dropdown')
+    .addEventListener('click', showMenu);
+    let liElements = [...document.querySelectorAll('ul > li')];
+
+    liElements.forEach(x => x.addEventListener('click', () => {
+        let boxElement = document.getElementById('box');
+        boxElement.style.backgroundColor = x.textContent;
+    }));
+
+    function showMenu() {
+    let ul = document.querySelector('#dropdown-ul');
+        if (ul.style.display == 'none') {
+            ul.style.display = 'block';
+        } else {
+            ul.style.display = 'none';
+            document.getElementById('box').style.backgroundColor = 'rgb(114, 112, 112)';
+        };
+    };
+};
--- Function Context/areaAndVolumeCalculator.js
+++ Function Context/areaAndVolumeCalculator.js
@@ -3,25 +3,18 @@ function area() {
     return this.x * this.y;
 };
 
-function vol(input) {
+function vol() {
     return this.x * this.y * this.z;
 };
 
 function solve(area, vol, input) {
-    input = JSON.parse(input);
-    let result = [];
-    input.forEach(element => {
-        let current = { 
-            area: Number(area.call(element)), 
-            volume: Number(vol.call(element))
-        };
-        if (current.area < 0) {
-            current.area = current.area * -1;
-        };
-        if (current.volume < 0) {
-            current.volume = current.volume * -1;
+    let figures = JSON.parse(input);
+
+    let result = figures.map(function(figure) {
+        return {
+            area: Math.abs(area.call(figure)),
+            volume: Math.abs(vol.call(figure))
         }
-        result.push(current);
     });
     return result;
 };
