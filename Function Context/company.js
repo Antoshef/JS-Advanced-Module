dec682203a208b2a2f624425866b2a2abdbd8001
Antoshef <antoshef21@gmail.com>
2021-01-09 18:53:58 +0200

    Context

--- Function Context/company.js
+++ Function Context/company.js
@@ -2,32 +2,73 @@ class Company {
     constructor() {
         this.departments = [];
     }
-    addEmployee(username, Salary, Position, Department) {
-            this.name = username,
-            this.salary = Salary,
-            this.position = Position,
-            this.department = Department;
 
-        if (this.name && this.salary > 0 && this.position && this.department) {
-            this.departments.push(Employee);
-            console.log(`New employee is hired. Name: ${this.name}. Position: ${this.position}`);
-        } else if (this.salary <= 0) {
-            console.log(' Invalid input!');
+    addEmployee(username, salary, position, department) {
+
+        if (username && salary >= 0 && position && department) {
+            if (this.departments[department]) {
+                this.departments[department].push({username, salary, position});
+            } else {
+                this.departments[department] = [];
+                this.departments[department].push({username, salary, position})
+            }
+            return `New employee is hired. Name: ${username}. Position: ${position}`;
+        } else if (salary < 0) {
+            return ' Invalid input!';
         } else {
-            console.log('Invalid input!');
+            return 'Invalid input!';
         }
     }
     bestDepartment() {
+        let best = {};
+        for (let line in this.departments) {
+            this.departments[line].forEach(x => {
+                if (best[line]) {
 
+                    best[line].count += 1;
+                    best[line].salary += x.salary;
+                } else {
+                    best[line] = {
+                        count: 1,
+                        salary: x.salary,
+                    };
+                }
+                best[line].average = best[line].salary / best[line].count;
+            });
+        };
+        let sorted = Object.entries(best).sort((a, b) => {
+            return b.average - a.average;
+        });
+        best = Array.from(this.departments[sorted[0][0]]);
+        let result = `Best Department is: ${sorted[0][0]}\n`;
+        let averagesalary = 0;
+        best.forEach(x => averagesalary += x.salary);
+        averagesalary /= best.length;
+        result += `Average salary: ${averagesalary.toFixed(2)}\n`;
+        sorted = Object.values(best).sort((a, b) => {
+            return (b.salary - a.salary) || (a.username.localeCompare(b.username));
+        });
+        for (let i = 0; i < sorted.length; i++) {
+            let current = sorted[i];
+            if (i == sorted.length - 1) {
+                result += `${current.username} ${current.salary} ${current.position}`;
+            } else {
+                result += `${current.username} ${current.salary} ${current.position}\n`;
+            };
+        };
+        return result;
     }
-}
+};
 
 let c = new Company();
-c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+
+let actual1 = c.addEmployee("Stanimir", 2000, "engineer", "Human resources");
+
 c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
 c.addEmployee("Slavi", 500, "dyer", "Construction");
 c.addEmployee("Stan", 2000, "architect", "Construction");
 c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
 c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
 c.addEmployee("Gosho", 1350, "HR", "Human resources");
-console.log(c.bestDepartment());
+
+let act = c.bestDepartment();
