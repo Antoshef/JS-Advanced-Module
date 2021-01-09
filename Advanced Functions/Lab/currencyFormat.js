e5120df2fea9b417935fb365b868215947154b1e
Antoshef <antoshef21@gmail.com>
2021-01-10 01:13:04 +0200

    Advanced

---
+++ Advanced Functions/Lab/currencyFormat.js
@@ -0,0 +1,19 @@
+let result = () => {
+    const separator = ",";
+    const symbol = "$";
+    const symbolFirst = false;
+
+    return function currencyFormatter(separator, symbol, symbolFirst, value) {
+        let result = Math.trunc(value) + separator;
+        result += value.toFixed(2).substr(-2,2);
+        if (symbolFirst) return symbol + ' ' + result;
+        else return result + ' ' + symbol;
+    }
+
+}; 
+
+
+let dollarFormatter = result(currencyFormatter);
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
