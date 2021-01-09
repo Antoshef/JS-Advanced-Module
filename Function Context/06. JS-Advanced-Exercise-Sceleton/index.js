2ad3df926e285b291fe8161afe4735f1e608a011
Antoshef <antoshef21@gmail.com>
2021-01-09 18:55:12 +0200

    Context

--- Function Context/06. JS-Advanced-Exercise-Sceleton/index.js
+++ Function Context/06. JS-Advanced-Exercise-Sceleton/index.js
@@ -1,3 +1,20 @@
 function solve(){
-   // TODO
+   
+   let tr = document.getElementsByTagName('tr');
+   let lastClicked;
+   [...tr].slice(1).forEach(row => {
+      row.addEventListener('click', (e) => {
+         let current = e.target.parentElement.style;
+         if (current.backgroundColor == 'rgb(65, 63, 94)') {
+            current.backgroundColor = '';
+         } else if (current.backgroundColor == '' || lastClicked == undefined) {
+            current.backgroundColor = 'rgb(65, 63, 94)';
+            if (!lastClicked) {
+               lastClicked = current;
+            }
+            lastClicked.backgroundColor = '';
+         }
+         lastClicked = current;
+      })
+   })
 }
