855c3ce38f0b7d66da3f015617af669485e22c45
Antoshef <antoshef21@gmail.com>
2021-01-25 15:29:20 +0200

    Advanced

--- Object Composition/Lab/test.js
+++ Object Composition/Lab/test.js
@@ -1,16 +1,19 @@
+function Person (fName,lName,age,location) {
+        this.firstName = fName,
+        this.lastName = lName,
+        this.age = age,
+        this.location = location,
+        this.name = function(input) {
+            papa = {obj: 20};
+            return papa
+        }
+    }
 
-let array = [
-    {name: 'Alex'},
-    {age: 25},
-    {job: 'Freelancer'},
-    {height: 185},
-]
-
-let result = array.reduce((acc, x) => {
-    return {...acc, ...x}
-},{});
-
-console.log(result);
-
-
-
+let mama = new Person('Sabina','Nikolova',60,'Yambol','Indonesia');
+console.log('he is ' + mama.name('tadjik'));
+console.log(mama.name[papa]);
+let txt = '';
+for (let x in mama) {
+    txt += mama[x] + ' ';
+}
+console.log(txt);
