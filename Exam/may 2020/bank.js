6b9368863b4224caba8fd0ee6e555108d745b4f7
Anton Stanev <antoshef21@gmail.com>
2021-01-28 22:07:46 +0200

    Exam

--- Exam/may 2020/bank.js
+++ Exam/may 2020/bank.js
@@ -1,21 +1,21 @@
 class Bank {
-    #bankName;
-
     constructor(bankName) {
-        this.#bankName = bankName,
-        this.allCustomers = []
+        this._bankName = bankName;
+        this.allCustomers = [];
     }
-
-    newCustomer = function({firstName, lastName, personalId}) {
-        if (bank.validation(personalId)) {
-            throw Error(`${firstName} ${lastName} is already our customer!`)
+    // Add new Customer
+    newCustomer = function(customer) {
+        if (this.allCustomers.some(x => x.personalId === customer.personalId)) {
+            throw Error(`${customer.firstName} ${customer.lastName} is already our customer!`)
         } else {
-            this.allCustomers.push({firstName, lastName, personalId, transactions: []});
-            return {firstName, lastName, personalId}
+            customer.transactions = [];
+            this.allCustomers.push(customer);
+            return customer
         }
     }
+    // Deposit Money
     depositMoney = function(personalId, amount) {
-        if (bank.validation(personalId)) {
+        if (this.allCustomers.some(x => x.personalId === personalId)) {
             let result = this.allCustomers.find(x => x.personalId == personalId);
             if (!result.hasOwnProperty('totalMoney')) {
                 result.totalMoney = 0;
@@ -27,20 +27,28 @@ class Bank {
             throw Error('We have no customer with this ID!');
         }
     }
+    // Withdraw Money
     withdrawMoney = function(personalId, amount) {
-        if (bank.validation(personalId)) {
+        if (this.allCustomers.some(x => x.personalId === personalId)) {
             let result = this.allCustomers.find(x => x.personalId === personalId);
             result.transactions.unshift(`${result.firstName} ${result.lastName} withdrew ${amount}$!`);
-            return bank.moneyCheck(personalId, amount);
+            let check = this.allCustomers.find(x => x.personalId === personalId);
+            if (check.totalMoney < amount) {
+                throw Error(`${check.firstName} ${check.lastName} does not have enough money to withdraw that amount!`);
+            } else {
+                check.totalMoney -= amount;
+                return check.totalMoney + '$';
+            };
         } else {
             throw Error('We have no customer with this ID!');
         }
     }
+    // Customer Info Details
     customerInfo = function(personalId) {
-        if (!bank.validation(personalId)) {
+        if (!this.allCustomers.some(x => x.personalId === personalId)) {
             throw Error('We have no customer with this ID!');
         } else {
-            let result = `Bank name: ${bank.#bankName}`;
+            let result = `Bank name: ${this._bankName}`;
             let customer = this.allCustomers.find(x => x.personalId === personalId);
             result += `\nCustomer name: ${customer.firstName} ${customer.lastName}`;
             result += `\nCustomer ID: ${customer.personalId}`;
@@ -51,24 +59,9 @@ class Bank {
                 result += '\n' + index + '. ' + x;
                 index--;
             });
-            return result
+            return result;
         }
     }
-
-    validation = function(personalId) {
-        return this.allCustomers.some(x => x.personalId === personalId);
-    }
-    
-    moneyCheck = function(personalId, amount) {
-        let result = this.allCustomers.find(x => x.personalId === personalId);
-        if (result.totalMoney < amount) {
-            throw Error(`${result.firstName} ${result.lastName} does not have enough money to withdraw that amount!`);
-        } else {
-            result.totalMoney -= amount;
-            return result.totalMoney + '$';
-        }
-    }
-    
 }
 
 let bank = new Bank("SoftUni Bank");
@@ -83,16 +76,3 @@ bank.depositMoney(4151596,555);
 console.log(bank.withdrawMoney(6233267, 125));
 
 console.log(bank.customerInfo(6233267));
-
-// { firstName: "Svetlin", lastName: "Nakov", personalId: 6233267 } 
-// { firstName: "Mihaela", lastName: "Mileva", personalId: 4151596 }
-// 500$
-// 375$
-// Bank name: SoftUni Bank
-// Customer name: Svetlin Nakov
-// Customer ID: 6233267
-// Total Money: 375$
-// Transactions:
-// 3. Svetlin Nakov withdrew 125$!
-// 2. Svetlin Nakov made depostit of 250$!
-// 1. Svetlin Nakov made depostit of 250$!
