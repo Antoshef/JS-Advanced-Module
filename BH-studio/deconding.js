624d5775a29b00bde4698276c8000c87511135e4
Anton Stanev <antoshef21@gmail.com>
2021-01-31 12:55:52 +0200

    bh

--- BH-studio/deconding.js
+++ BH-studio/deconding.js
@@ -1,30 +1,20 @@
-function decoding(inputParameters) {
+function decodingSoftware(inputParameters) {
+    let result = 0;
+    let recievedMessages = [];
 
     // Recieve Docking Parameters
-    let bitMask = inputParameters.shift().split('mask = ')[1];
-    let code = {};
-    let result = 0;
-
-    // Store Data
-    inputParameters.map(line => {
-        let [memory, entry] = line.split(' = ');
-        memory = memory.match(/[\d+]/g).join('');
-        code[memory] = {
-            input: parseInt(entry),
-            binary: toBinary(parseInt(entry)),
-            output: 0,
-        };
-    });
-
-    // Iterate through Input Codes
-    for (let key in code) {
-        code[key].output = applyMask(bitMask, code[key].binary);
-        result += code[key].output;
+    while (inputParameters.length > 0) {
+        let currentParameter = inputParameters.shift();
+        let bitMask = currentParameter.shift().split('mask = ')[1];
+        let code = {};
+        recievedMessages.push(storeData(currentParameter, code, bitMask));
     };
     
-    // Printing Result
-    console.log(result);
-}
+    // Return Result
+    let recievedMessageOutputs = calculateCoordinates(recievedMessages);
+    result = recievedMessageOutputs.reduce((acc , x) => acc + x);
+    return result;
+};
 
 // Apply Mask to each Line of Code
 function applyMask(bitMask, currentBinary) {
@@ -41,6 +31,21 @@ function applyMask(bitMask, currentBinary) {
     return parseInt(currentBinary, 2);
 };
 
+// Store Data in Object
+function storeData(parameter, code, bitMask) {
+    parameter.map(line => {
+    let [memory, entry] = line.split(' = ');
+    memory = memory.match(/[\d+]/g).join('');
+    let binaryCode = toBinary(parseInt(entry));
+    code[memory] = {
+        input: parseInt(entry),
+        binary: binaryCode,
+        output: applyMask(bitMask, binaryCode),
+        };
+    });
+    return code;
+};    
+
 // Convert Decimal to Binary
 function toBinary(n) {
     let binary = parseInt(n, 10).toString(2);
@@ -48,12 +53,43 @@ function toBinary(n) {
     return fill.concat(binary);
 };
 
+// Sum Coordinates
+function calculateCoordinates(recievedMessages) {
+    return recievedMessages.map(message => {
+        return Object.values(message).reduce(((acc, x) => acc + x.output), 0);
+    });
+};
 
-decoding(
+decodingSoftware(
     [
-        'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
-        'mem[8] = 11',
-        'mem[7] = 101',
-        'mem[8] = 0',
+        [
+            'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+            'mem[8] = 11',
+            'mem[7] = 101',
+            'mem[8] = 0'
+        ],
+        [
+            'mask = 0010X0X100X011101X010X001110X00X1101',
+            'mem[46114] = 15140',
+            'mem[38354] = 6246405',
+            'mem[3227] = 1976',
+            'mem[46579] = 414875',
+            'mem[20140] = 185761241'
+        ],
+        [
+            'mask = 0X00100000X0X01001X001111X101011X110',
+            'mem[11688] = 122943295',
+            'mem[54669] = 820167',
+            'mem[1156] = 12904',
+            'mem[36883] = 436481745',
+            'mem[49687] = 12497'
+        ],
+        [
+            'mask = 000XX01000X01010100100X000X01110110X',
+            'mem[10002] = 18361339',
+            'mem[50031] = 261',
+            'mem[16419] = 537704',
+            'mem[48032] = 2783140'
+        ]
     ]
 )
--- BH-studio/encrypt.js
+++ BH-studio/encrypt.js
@@ -1,21 +1,52 @@
 // Creating Class to Store Messages and Methods
-class Message {
-    constructor(mask) {
-        this.mask = mask;
-        this.code = {}
-    }
+let commands = {
+    splitMessages(input) {
+        let inputParameters = input.split('\n');
+        let result = [];
+        let current = [];
+        current.push(inputParameters.shift());
+        inputParameters.map(x => {
+            if (x.includes('mask')) {
+                result.push(current);
+                current = [];
+                current.push(x);
+            } else {
+                current.push(x);
+            }
+        })
+        result.push(current);
+        return result;
+    },
+
+    readMessage(inputParameters) {
+        let code = {};
+        inputParameters.map(x => {
+            let mask = x.shift().split(' = ')[1];
+            x.map(line => {
+                let [command, entry] = line.split(' = ');
+                memory = command.match(/[\d+]/g).join('');
+                let binary = commands.toBinary(parseInt(entry));
+                code[memory] = {
+                    input: parseInt(entry),
+                    binary: binary,
+                    output: commands.applyMask(mask, binary),
+                };    
+            });
+        });
+        return code;
+    },
 
     // Convert Decimal to Binary
     toBinary(n) {
         let binary = parseInt(n, 10).toString(2);
         let fill = '0'.repeat((36 - binary.length));
         return fill.concat(binary);
-    }
+    },
 
     // Convert Binary to Decimal
     toDecimal(current) {
         return parseInt(current, 2);
-    }
+    },
 
     // Transform Binary input using Mask
     applyMask(bitMask, binary) {
@@ -28,56 +59,27 @@ class Message {
         }
         binary = binary.join('');
         return this.toDecimal(binary);
-    }
+    },
 };
 
 function dockingSoftware(input) {
-
     // Recieve Docking Parameters
-    let inputParameters = input.split('\n');
     let recievedMessages = [];
     let programResult = 0;
 
+    let allMessages = commands.splitMessages(input);
+
     // Read Current Message
-    let currentMessage;
-    inputParameters.map(line => {
-        let [command, entry] = line.split(' = ');
-        if (command.includes('mask')) {
-            currentMessage = new Message(entry);
-            recievedMessages.push(currentMessage);
-        } else {
-            memory = command.match(/[\d+]/g).join('');
-            currentMessage.code[memory] = {
-                input: parseInt(entry),
-                binary: currentMessage.toBinary(parseInt(entry)),
-                output: 0,
-            };
-        };
-    });
-    
-    // Interate through each Recieved Message
-    for (let dockingParameters of recievedMessages) {
-        let currentMessageResult = 0;
-        let currentMask = dockingParameters.mask;
-        for (let memoryCode in dockingParameters.code) {
-            let currentOutput = dockingParameters.code[memoryCode].output
-            let currentBinary = dockingParameters.code[memoryCode].binary;
-
-            // Applying Mask to each Message Recieved
-            currentOutput = currentMessage.applyMask(currentMask, currentBinary);
-            currentMessageResult += currentOutput;
-        }
-        // Apply each Message Result to Final Result
-        programResult += currentMessageResult;
-    };
-
-    // Printing Result
-    console.log(programResult);
+    let currentMessage = commands.readMessage(allMessages);
+    recievedMessages.push(currentMessage);
 };
 
 dockingSoftware(
     `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
     mem[8] = 11
     mem[7] = 101
-    mem[8] = 0`
+    mem[8] = 0
+    mask = 00X01010000010101X010X01X100010011X0
+    mem[23911] = 21827
+    mem[37956] = 3727083`
 )
