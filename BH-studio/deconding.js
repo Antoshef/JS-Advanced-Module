5ad03775ec85b7f272ab693233131509a4708e12
Anton Stanev <antoshef21@gmail.com>
2021-01-31 09:41:19 +0200

    bh

--- BH-studio/deconding.js
+++ BH-studio/deconding.js
@@ -8,9 +8,7 @@ function decoding(inputParameters) {
     // Store Data
     inputParameters.map(line => {
         let [memory, entry] = line.split(' = ');
-        memory = memory
-        .match(/[\d+]/g)
-        .join('');
+        memory = memory.match(/[\d+]/g).join('');
         code[memory] = {
             input: parseInt(entry),
             binary: toBinary(parseInt(entry)),
@@ -20,45 +18,42 @@ function decoding(inputParameters) {
 
     // Iterate through Input Codes
     for (let key in code) {
-        code[key].output = maskElements(bitMask, code[key].binary);
+        code[key].output = applyMask(bitMask, code[key].binary);
         result += code[key].output;
-    }
+    };
     
     // Printing Result
     console.log(result);
 }
 
-// Masking each Element
-function maskElements(bitMask, currentBinary) {
-    let splitMask = bitMask.split('');
+// Apply Mask to each Line of Code
+function applyMask(bitMask, currentBinary) {
+    bitMask = bitMask.split('');
     currentBinary = currentBinary.split('');
     for (let i = 0; i < 36; i++) {
-        if (splitMask[i] !== 'X') {
-            currentBinary[i] = splitMask[i];
-        }
-    }
+        if (bitMask[i] !== 'X') {
+            currentBinary[i] = bitMask[i];
+        };
+    };
     currentBinary = currentBinary.join('');
 
     // Convert Binary to Decimal
     return parseInt(currentBinary, 2);
-}
+};
 
 // Convert Decimal to Binary
 function toBinary(n) {
     let binary = parseInt(n, 10).toString(2);
     let fill = '0'.repeat((36 - binary.length));
     return fill.concat(binary);
-}
+};
 
 
 decoding(
     [
-        'mask = X0000X00X11010X011011010X11X000000X1',
-        'mem[46536] = 230918555',
-        'mem[45646] = 293515330',
-        'mem[34129] = 72857965',
-        'mem[28342] = 3005',
-        'mem[1998] = 12034321',
-        'mem[9738] = 10515'
+        'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+        'mem[8] = 11',
+        'mem[7] = 101',
+        'mem[8] = 0',
     ]
 )
--- BH-studio/encrypt.js
+++ BH-studio/encrypt.js
@@ -1,3 +1,36 @@
+// Creating Class to Store Messages and Methods
+class Message {
+    constructor(mask) {
+        this.mask = mask;
+        this.code = {}
+    }
+
+    // Convert Decimal to Binary
+    toBinary(n) {
+        let binary = parseInt(n, 10).toString(2);
+        let fill = '0'.repeat((36 - binary.length));
+        return fill.concat(binary);
+    }
+
+    // Convert Binary to Decimal
+    toDecimal(current) {
+        return parseInt(current, 2);
+    }
+
+    // Transform Binary input using Mask
+    applyMask(bitMask, binary) {
+        let splitMask = bitMask.split('');
+        binary = binary.split('');
+        for (let i = 0; i < 36; i++) {
+            if (splitMask[i] !== 'X') {
+                binary[i] = splitMask[i];
+            }
+        }
+        binary = binary.join('');
+        return this.toDecimal(binary);
+    }
+};
+
 function dockingSoftware(input) {
 
     // Recieve Docking Parameters
@@ -5,50 +38,15 @@ function dockingSoftware(input) {
     let recievedMessages = [];
     let programResult = 0;
 
-    // Creating Class to Store Messages and Methods
-    class Message {
-        constructor(mask) {
-            this.mask = mask;
-            this.code = {}
-        }
-
-        // Convert Decimal to Binary
-        toBinary(n) {
-            let binary = parseInt(n, 10).toString(2);
-            let fill = '0'.repeat((36 - binary.length));
-            return fill.concat(binary);
-        }
-
-        // Convert Binary to Decimal
-        toDecimal(current) {
-            return parseInt(current, 2);
-        }
-
-        // Transform Binary input using Mask
-        applyMask(mask, current) {
-            let splitMask = mask.split('');
-            current = current.split('');
-            for (let i = 0; i < 36; i++) {
-                if (splitMask[i] !== 'X') {
-                    current[i] = splitMask[i];
-                }
-            }
-            current = current.join('');
-            return this.toDecimal(current);
-        }
-    };
-
     // Read Current Message
     let currentMessage;
-    inputParameters.forEach(line => {
+    inputParameters.map(line => {
         let [command, entry] = line.split(' = ');
         if (command.includes('mask')) {
             currentMessage = new Message(entry);
             recievedMessages.push(currentMessage);
         } else {
-            memory = command
-            .match(/[\d+]/g)
-            .join('');
+            memory = command.match(/[\d+]/g).join('');
             currentMessage.code[memory] = {
                 input: parseInt(entry),
                 binary: currentMessage.toBinary(parseInt(entry)),
@@ -61,9 +59,9 @@ function dockingSoftware(input) {
     for (let dockingParameters of recievedMessages) {
         let currentMessageResult = 0;
         let currentMask = dockingParameters.mask;
-        for (let singleMemoryCode in dockingParameters.code) {
-            let currentOutput = dockingParameters.code[singleMemoryCode].output
-            let currentBinary = dockingParameters.code[singleMemoryCode].binary;
+        for (let memoryCode in dockingParameters.code) {
+            let currentOutput = dockingParameters.code[memoryCode].output
+            let currentBinary = dockingParameters.code[memoryCode].binary;
 
             // Applying Mask to each Message Recieved
             currentOutput = currentMessage.applyMask(currentMask, currentBinary);
@@ -78,22 +76,8 @@ function dockingSoftware(input) {
 };
 
 dockingSoftware(
-    `mask = XXX0X0X00X00X0X0X0XX0XXX001XX00101X0
-    mem[26794] = 326029
-    mem[26794] = 3479
-    mem[26794] = 5229418
-    mem[26794] = 59518329
-    mem[26794] = 4021
-    mask = 000000X1X1110X10X100001X001X00X1X101
-    mem[13785] = 14683
-    mem[22801] = 63583
-    mem[15888] = 309486967
-    mem[16644] = 1751
-    mask = X0000X00XXX010X011011010X11X000000X1
-    mem[46536] = 230918555
-    mem[45646] = 293515330
-    mem[46536] = 72857965
-    mem[28342] = 3005
-    mem[1998] = 12034321
-    mem[9738] = 10515`
+    `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+    mem[8] = 11
+    mem[7] = 101
+    mem[8] = 0`
 )
